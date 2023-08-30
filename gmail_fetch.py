ba73608c710ac528f51a4a59ff93d38590ea1b03
KevKibe <kchegz234@gmail.com>
2023-08-30 17:55:11 +0300

    update

--- gmail_fetch.py
+++ gmail_fetch.py
@@ -3,101 +3,59 @@ from googleapiclient.discovery import build
 from base64 import urlsafe_b64decode
 from datetime import datetime, timedelta
 
+class GmailAPI:
+    def __init__(self):
+        self.credentials = authenticate()
+        self.service = build('gmail', 'v1', credentials=self.credentials)
 
-credentials = authenticate()
+    def get_email_content(self, message_id):
+        """Gets the content of a specific email using the Gmail API."""
+        message = self.service.users().messages().get(userId='me', id=message_id, format='full').execute()
+        payload = message['payload']
+        headers = payload['headers']
 
+        headers_dict = {header['name']: header['value'] for header in headers}
 
+        subject = headers_dict.get('Subject', 'No Subject')
+        from_header = headers_dict.get('From', 'Unknown Sender')
+        date_header = headers_dict.get('Date', 'Unknown Date')
 
+        body_data = ""
+        if 'body' in payload and 'data' in payload['body']:
+            body_data = payload['body']['data']
+            body = urlsafe_b64decode(body_data).decode('utf-8')
+        else:
+            body = "No content"
 
-def get_email_content(message_id, service):
-    """Gets the content of a specific email using the Gmail API."""
-    message = service.users().messages().get(userId='me', id=message_id, format='full').execute()
-    payload = message['payload']
-    headers = payload['headers']
-    subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject')
-    
-    from_header = next((header['value'] for header in headers if header['name'] == 'From'), 'Unknown Sender')
-    
-    date_header = next((header['value'] for header in headers if header['name'] == 'Date'), 'Unknown Date')
-    # date = format_date(date_header)
-    
-    body_data = ""
-    if 'body' in payload and 'data' in payload['body']:
-        body_data = payload['body']['data']
-        body = urlsafe_b64decode(body_data).decode('utf-8')
-    else:
-        body = "No content"
+        label_ids = message.get('labelIds', [])
 
-    label_ids = message.get('labelIds', [])
-    
-    return {
-        'From': from_header,
-        'Date': date_header,
-        'Subject': subject,
-        'Body': body,
-        'Labels': label_ids
-    }
+        return {
+            'From': from_header,
+            'Date': date_header,
+            'Subject': subject,
+            'Body': body,
+            'Labels': label_ids
+        }
 
+    def get_emails(self, days_ago):
+        """Gets and prints the content of emails from the past specified days in the Gmail inbox using the Gmail API."""
+        today = datetime.now()
+        target_date = today - timedelta(days=days_ago)
+        formatted_date = target_date.strftime('%Y-%m-%d')
+        query = f'is:inbox after:{formatted_date}'
 
-# def get_email_content(message_id, service):
-#     """Gets the content of a specific email using the Gmail API."""
-    
-#     message = service.users().messages().get(userId='me', id=message_id, format='full').execute()
-#     payload = message['payload']
-#     headers = payload['headers']
-    
-#     # Convert headers to a dictionary for easier access
-#     headers_dict = {header['name']: header['value'] for header in headers}
-    
-#     subject = headers_dict.get('Subject', 'No Subject')
-#     from_header = headers_dict.get('From', 'Unknown Sender')
-#     date_header = headers_dict.get('Date', 'Unknown Date')
+        results = self.service.users().messages().list(userId='me', q=query).execute()
+        messages = results.get('messages', [])
 
-#     body_data = ""
-#     if 'body' in payload and 'data' in payload['body']:
-#         body_data = payload['body']['data']
-#         body = urlsafe_b64decode(body_data).decode('utf-8')
-#     else:
-#         body = "No content"
-
-#     label_ids = message.get('labelIds', [])
-    
-#     return {
-#         'From': from_header,
-#         'Date': date_header,
-#         'Subject': subject,
-#         'Body': body,
-#         'Labels': label_ids
-#     }
-
-
-def get_emails(days_ago):
-    """Gets and prints the content of emails from the past specified days in the Gmail inbox using the Gmail API."""
-    service = build('gmail', 'v1', credentials=credentials)
-    
-    # Calculate the date 'days_ago' days ago
-    today = datetime.now()
-    target_date = today - timedelta(days=days_ago)
-    formatted_date = target_date.strftime('%Y-%m-%d')
-    
-    query = f'is:inbox after:{formatted_date}'
-    
-    results = service.users().messages().list(userId='me', q=query).execute()
-    messages = results.get('messages', [])
-
-
-    if not messages:
-        return []  # Return an empty list if no emails found
-    else:
         email_data_list = []
         for message in messages:
             message_id = message['id']
-            email_data = get_email_content(message_id, service)
+            email_data = self.get_email_content(message_id)
             email_data_list.append(email_data)
-        
+
         return email_data_list
 
-
-
-email_data_list = get_emails(1)
-print(email_data_list)
+    # gmail_api = GmailAPI()
+    # email_data_list = gmail_api.get_emails(2)
+    # for email_data in email_data_list:
+    #     print(email_data)
--- text_preprocess.py
+++ text_preprocess.py
@@ -1,25 +1,49 @@
-from langchain.text_splitter import CharacterTextSplitter  # Make sure to import the necessary module
-from gmail_fetch import get_emails
-import re 
+from langchain.text_splitter import CharacterTextSplitter
+from gmail_fetch import GmailAPI
+import re
 
+class TextProcessor:
+    def __init__(self):
+        self.url_pattern = re.compile(r'https?://\S+|www\.\S+')
+        self.text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
 
+    def remove_html_tags(self, text):
+        """Removes all HTML tags from the text"""
+        return re.sub(r'<[^>]+>', '', text)
 
-def get_text_chunks_from_email_data(email_data):
+    def remove_links(self, text):
+        """Replaces URLs with an empty string"""
+        text_without_links = self.url_pattern.sub('', text)
+        return text_without_links
+
+    def preprocess_email_data(self, email_data):
+        preprocessed_body = self.remove_html_tags(email_data['Body'])
+        processed_email_data = {
+            'From': email_data['From'],
+            'Date': email_data['Date'],
+            'Subject': email_data['Subject'],
+            'Body': preprocessed_body,
+            'Labels': email_data['Labels']
+        }
+        return processed_email_data
+
+    def get_text_chunks_from_email_data(self, processed_email_data):
+        text_chunks = self.text_splitter.split_text(processed_email_data['Body'])
+        return text_chunks
+
+    # text_processor = TextProcessor()
+    # gmail_api = GmailAPI()
+    # email_data_list = gmail_api.get_emails(2)
     
-    preprocessed_body = email_data['Body']  # Use the preprocessed body from email data
-    text_splitter = CharacterTextSplitter(
-         separator="\n",
-         chunk_size=1000,
-         chunk_overlap=200,
-         length_function=len
-    )
-    text_chunks = text_splitter.split_text(preprocessed_body)
-    return text_chunks
 
-
-email_data_list = get_emails(2)
-
-for email_data in email_data_list:
-    text_chunks = get_text_chunks_from_email_data(email_data)
-    
-    print(text_chunks)
+    # for email_data in email_data_list:
+    #     processed_email_data = text_processor.preprocess_email_data(email_data)
+    #     text_chunks = text_processor.get_text_chunks_from_email_data(processed_email_data)
+        
+    #     for chunk in text_chunks:
+    #         print(chunk)
