4ffb9eac5fd8d34fd0e9743505318e2d6072a31c
astianmuchui <astianmuchui@gmail.com>
2023-10-02 08:13:52 +0300

    added AOS

--- UI_V2/assets/js/type.js
+++ UI_V2/assets/js/type.js
@@ -30,11 +30,11 @@ TypeWriter.prototype.type = function() /* Type method */
   
       this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-      let typeSpeed = 30; /** Reduce this to increase speed  */
+      typeSpeed = 80; /** Reduce this to increase speed  */
       
       if (this.isDeleting)
       {
-          typeSpeed /= 2;
+          typeSpeed /= 2.5;
       }
       
       if (!this.isDeleting && this.txt === fullTxt)
@@ -46,9 +46,10 @@ TypeWriter.prototype.type = function() /* Type method */
       {
           this.isDeleting = false;
           this.wordIndex++;
-          typeSpeed = 500;
+          typeSpeed = 50;
       }
 
+      
       setTimeout(() => this.type(), typeSpeed);
   }
 
@@ -62,4 +63,5 @@ TypeWriter.prototype.type = function() /* Type method */
         const wait = txtElement.getAttribute('data-wait');
         new TypeWriter(txtElement, words, wait);
     });
+
 }
